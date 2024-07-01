d1ae5af796f482c38a15580edcfc2cde3d8c69bf
Anusara97 <anusara777rcm@gmail.com>
2024-07-01 20:31:09 +0530

    Q1.c is modified

--- Ass10/Q1.c
+++ Ass10/Q1.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main () {
+    char ch [] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+
+    for (int i=0; i<10; i++) {
+        printf("%c ", ch[i]);
+    }
+    printf("\n");
+    return 0;
+}
