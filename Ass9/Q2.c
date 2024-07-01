1ffdfbd85cb19297451f1c9cabc0299898016e07
Anusara97 <anusara777rcm@gmail.com>
2024-07-01 10:16:52 +0530

    Q2.c is modified

--- Ass9/Q2.c
+++ Ass9/Q2.c
@@ -2,5 +2,8 @@
 
 int main() {
     printf("size of a short is %d\n", sizeof(short));
+    printf("size of a int is %d\n", sizeof(int));
+    printf("size of a char is %d\n", sizeof(char));
+    printf("size of a long is %d\n", sizeof(long));
     return 0;
 }
