ccca177c1c45752cfecb2c4e4fdb40f892ee09fe
Anusara97 <anusara777rcm@gmail.com>
2024-07-02 12:19:33 +0530

    Q4.c is created

--- Ass10/Q4.c
+++ Ass10/Q4.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int my_array[] = {1, 23, 17, 4, -5, 100};
+int *ptr;
 
+int main () {
+    int i;
+    ptr = &my_array[0];
+    printf("\n\n");
+    for (i=0; i<6; i++) {
+        printf("my_array[%d] = %d ", i,my_array[i]);
+        printf("ptr + %d = %d\n",i, *ptr++);
+    }
+    return 0;
+}
