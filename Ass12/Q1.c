98caf873acae079359d3be37d4e4f55601d71408
Anusara97 <anusara777rcm@gmail.com>
2024-07-04 08:45:12 +0530

    Q1 is Done

--- Ass12/Q1.c
+++ Ass12/Q1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <string.h>
 
+typedef struct lib_books {
+    char title[20];
+    char author[15];
+    int pages;
+    float price;
+}lib_books;
+
+int main() {
+    lib_books book1 = {"Introduction to C", "Arther", 200,560.00};
+    printf("====Book Details====");
+    printf("\nTitle: %s\nAuthor: %s\nPages: %d\nPrice: %.2f\n", book1.title, book1.author, book1.pages, book1.price);
+    return 0;
+}
